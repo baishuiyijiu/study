909d095d7ccd12b8ceb9d673c927e5c40044c795
huangli <baishuiyijiu@hotmail.com>
2019-07-03 10:42:43 +0800

    refactor shoppingList

--- shoppingList/javaScript/shoppingList.js
+++ shoppingList/javaScript/shoppingList.js
@@ -1,80 +1,122 @@
 function shopping() {
-	function Product(id, name, price, unit) {
-	this.id = id;
-	this.name = name;
-	this.price = price;
-	this.unit = unit;
-	}
-	//购物码list
-	var itemInput = ["ITEM000001", "ITEM000001", "ITEM000001", "ITEM000002-5", "ITEM000003-2", "ITEM000003", "ITEM000004"];
-
-	//商品优惠码list
-	var discountItem = ["ITEM000001", "ITEM000002"];
-	var buyTwoAndGiveOneItem = ["ITEM000001", "ITEM0000021"];
-
-	//商品类
-	var productStore = new Map();
-	productStore.set("ITEM000001", new Product("ITEM000001", "苹果", 5.5, "斤"));
-	productStore.set("ITEM000002", new Product("ITEM000002", "羽毛球", 1, "个"));
-	productStore.set("ITEM000003", new Product("ITEM000003", "可口可乐", 2, "瓶"));
-	productStore.set("ITEM000004", new Product("ITEM000004", "巧克力", 10, "盒"));
-
-	var shoppingList = new Map();
-	var discountInList = new Map();
-	var discountText = "买二赠一商品: ";
-	var actualPaidSum = 0;
-	var discountSum = 0;
-	var DISCOUNT = 0.95;
-
-	//购物列表
-	for (var i = 0; i < itemInput.length; i++) {
-	    var item = itemInput[i];
-	    var v = item.search("-");
-	    if (v == -1) {
-	        shoppingList.has(item) ? shoppingList.set(item, shoppingList.get(item) + 1) : shoppingList.set(item, 1);
-	    } else {
-	    	var itemId = item.substr(0, v);
-	    	var itemNum = item.substr(v+1);
-	        shoppingList.has(itemId) ? shoppingList.set(itemId, shoppingList.get(item) + parseInt(itemNum)) : shoppingList.set(itemId, parseInt(itemNum));
-	    }
-	}
-
-	//返回满二赠一所需付款的实际数量
-	function actualPaidNum(productId, productNum) {
-		return buyTwoAndGiveOneItem.indexOf(productId) == -1 ? productNum : Math.floor(productNum / 3) * 2 + productNum % 3;
-	}
-
-	//实际付款价格
-	function actualPaidMoney(productId, productNum, price) {
-		if (buyTwoAndGiveOneItem.indexOf(productId) != -1) {
-			return actualPaidNum(productId, productNum) * price;
-		} else if(discountItem.indexOf(productId) != -1) {
-			return parseFloat((productNum * price * DISCOUNT).toFixed(2));
+	 class Product {
+		constructor(id, name, price, unit) {
+			this.id = id;
+			this.name = name;
+			this.price = price;
+			this.unit = unit;
 		}
-		return productNum * price;
+	}
+
+	class ProductStore {
+		constructor() {
+			this.discountItems = [];
+			this.buyTwoAndGiveOneItems = [];
+			this.products = new Map();
+			this.DISCOUNT = 0.95;
+		}
+
+		getProduct(id) {
+			return this.products.get(id);
+		}
+
+		addDiscountItem(id) {
+			this.discountItems.push(id);
+		}
+
+		addBuyTwoAndGiveOneItem(id) {
+			this.buyTwoAndGiveOneItems.push(id)
+		}
+
+		addProduct(product) {
+			this.products.set(product.id, product);
+		}
+
+		isDiscountItem(id) {
+			return this.discountItems.indexOf(id) != -1;
+		}
+
+		isBuyTwoAndGiveOneItem(id) {
+			return this.buyTwoAndGiveOneItems.indexOf(id) != -1;
+		}
+	}
+
+	var productStore = new ProductStore();
+	productStore.addDiscountItem("ITEM000001");
+	productStore.addDiscountItem("ITEM000002");
+	productStore.addBuyTwoAndGiveOneItem("ITEM000001");
+	productStore.addBuyTwoAndGiveOneItem("ITEM000004");
+
+	productStore.addProduct(new Product("ITEM000001", "苹果", 5.5, "斤"));
+	productStore.addProduct(new Product("ITEM000002", "羽毛球", 1, "个"));
+	productStore.addProduct(new Product("ITEM000003", "可口可乐", 2, "瓶"));
+	productStore.addProduct(new Product("ITEM000004", "巧克力", 10, "盒"));
+
+	//购物清单
+	var itemInput = ["ITEM000001", "ITEM000001", "ITEM000001", "ITEM000002-5", "ITEM000003-2", "ITEM000003", "ITEM000004"];
+	
+	//统计购物清单中商品的种类和数量
+	function parseShoppingList(itemInput) {
+		var list = new Map();
+		for (var i = 0; i < itemInput.length; i++) {
+		    var item = itemInput[i];
+		    var pos = item.search("-");
+		    if (pos == -1) {
+		    	var id = item;
+		    	var num = 1;
+		    } else {
+		    	var id = item.substr(0, pos);
+		    	var num = parseInt(item.substr(pos+1));
+		    }
+		    list.has(id) ? list.set(id, list.get(id) + num) : list.set(id, num);
+		}
+		return list;
+	}
+
+	var shoppingList = parseShoppingList(itemInput);
+	var actualNumToBePaied = new Map();
+	var actualMoneyToBePaied = new Map();
+
+	//计算每种商品实际需付款的数量和价格
+	for (var [id, num] of shoppingList) {
+		var price = productStore.getProduct(id).price;
+		var actualNum = num;
+		var actaulPrice = actualNum * price;
+		if (productStore.isBuyTwoAndGiveOneItem(id)) {
+			actualNum = Math.floor(num / 3) * 2 + num % 3;
+			actaulPrice = actualNum * price;
+		} else if (productStore.isDiscountItem(id)) {
+			actaulPrice = actaulPrice * productStore.DISCOUNT;
+		}
+		actualNumToBePaied.set(id, actualNum);
+		actualMoneyToBePaied.set(id, actaulPrice);
 	}
 
 	//采购清单打印
-	var totalPrice = 0;
+	var savedPriceSum = 0;
+	var actualPaidSum = 0;
+	var discountText = "买二赠一商品: ";
 	var totalProductText = "所购物品如下：";
 	for (var [itemId, itemNum] of shoppingList) {
-		var product = productStore.get(itemId);
-		var paid = actualPaidMoney(itemId, itemNum, product.price);
+		var product = productStore.getProduct(itemId);
+		var paid = actualMoneyToBePaied.get(itemId);
+		var savedPrice = product.price * itemNum - paid;
 		totalProductText = totalProductText + "<br>" + "<br>" + "名称： " + product.name + "， 数量: " + itemNum + product.unit + "， 单价： " + product.price + "（元）， 小计： " + paid + "（元）";
-		if (buyTwoAndGiveOneItem.indexOf(itemId) != -1 || discountItem.indexOf(itemId) != -1) {
-			if (buyTwoAndGiveOneItem.indexOf(itemId) != -1) {
-				var discountNum = itemNum - actualPaidNum(itemId, itemNum);
+		if (productStore.isDiscountItem(itemId) || productStore.isDiscountItem(itemId)) {
+			if (productStore.isDiscountItem(itemId)) {
+				var discountNum = itemNum - actualNumToBePaied.get(itemId);
 				discountText = discountText + "<br>" + "<br>" + "名称： " + product.name + "数量： " + discountNum + product.unit;
 			}
-			totalProductText = totalProductText + "， 节省： " + (product.price * itemNum - paid) + "（元）";
+			totalProductText = totalProductText + "， 节省： " + savedPrice + "（元）";
 		}
 
 		actualPaidSum = actualPaidSum + paid; 
-		totalPrice = totalPrice + product.price * itemNum;
+		savedPriceSum = savedPriceSum + savedPrice;
 	}
 
-	// 显示对象中的数据：
+	//显示数据：
 	document.getElementById("productdetail").innerHTML = totalProductText;
 	document.getElementById("discountdetail").innerHTML = discountText;
-	document.getElementById("total").innerHTML = "总价：" + actualPaidSum + "（元）" + "<br>" + "<br>" + "节省： " + (totalPrice - actualPaidSum).toFixed(2) + "（元）";
+	document.getElementById("total").innerHTML = "总价：" + actualPaidSum + "（元）" + "<br>" + "<br>" + "节省： " + savedPriceSum.toFixed(2) + "（元）";
+
 }
