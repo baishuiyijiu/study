30e8cb9f4c148e90ba2713d87d1766b244de666c
huangli <baishuiyijiu@hotmail.com>
2019-07-28 18:18:29 +0800

    mock static block

--- mock-seed/src/test/java/com/xxx/xxx/service/PersonServiceTest.java
+++ mock-seed/src/test/java/com/xxx/xxx/service/PersonServiceTest.java
@@ -3,21 +3,24 @@ package com.xxx.xxx.service;
 import com.xxx.xxx.domain.Person;
 import com.xxx.xxx.domain.PersonRequest;
 import com.xxx.xxx.utils.SalaryCalculator;
+import com.xxx.xxx.utils.TimeUnit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.omg.CORBA.PolicyError;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(SalaryCalculator.class)
+@PrepareForTest(value = {SalaryCalculator.class, TimeUnit.class})
+@SuppressStaticInitializationFor("com.xxx.xxx.utils.SalaryCalculator")
 public class PersonServiceTest {
 
     @InjectMocks
@@ -37,9 +40,10 @@ public class PersonServiceTest {
     public void should_find_person() {
         //given
         PowerMockito.mockStatic(SalaryCalculator.class);
+        PowerMockito.mockStatic(TimeUnit.class);
 
         //when
-        PowerMockito.when(SalaryCalculator.calculate(BigDecimal.TEN)).thenReturn(BigDecimal.valueOf(20));
+        PowerMockito.when(SalaryCalculator.calculate(any())).thenReturn(BigDecimal.valueOf(20));
 
         //then
         assertThat(personService.find(new PersonRequest("James"))).
